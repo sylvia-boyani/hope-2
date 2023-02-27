5884fb716afc82444e59c9200d49668de0fddbf5
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-02-27 23:28:50 +0000

    edit home & Statement pages

--- src/routes/AboutWho.js
+++ src/routes/AboutWho.js
@@ -1,5 +1,19 @@
-const AboutWho = () => {
-  return <h2>Who we are</h2>;
-};
+import Card from 'react-bootstrap/Card';
 
-export default AboutWho;
+function ImgOverlayExample() {
+  return (
+    <Card className="bg-dark text-white">
+      <Card.Img src="holder.js/100px270" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+  );
+}
+
+export default ImgOverlayExample;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,24 @@
-const Home = () => {
-  return <h2>Home page content</h2>;
-};
+import React from 'react'
+import video from '../assets/video1.mp4'
+import './Home.css'
 
-export default Home;
+
+function Home() {
+  return (
+    <div>
+      <video id ="vida" src={video} autoPlay loop muted />
+      <div className="container header_container">
+        <h5>Hello, Welcome to</h5>
+        <h1>Tumaini Ministries Centre</h1>
+        <h2><emp>You have been made clean already by the teaching I have given you.</emp></h2>
+        <h2>JOHN 15:3</h2>
+        <h3 className= "text-light">Needing Jesus Christ Together</h3>
+        {/* <img id ="logo" src={tuma} alt="" /> */}
+        {/* <HeaderSocial /> */}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home

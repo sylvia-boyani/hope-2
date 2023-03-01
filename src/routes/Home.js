7784734eb9fefd2ec263b875cf9d84b7cdab2126
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-03-01 12:44:32 +0000

    done with header section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,12 +6,14 @@ import './Home.css'
 function Home() {
   return (
     <div>
-      <video id ="vida" src={video} autoPlay loop muted />
+     <div id='overlay'>
+     <video id ="vida" src={video} autoPlay loop muted />
+     </div>
       <div className="container header_container">
         <h5>Hello, Welcome to</h5>
         <h1>Tumaini Ministries Centre</h1>
-        <h2><emp>You have been made clean already by the teaching I have given you.</emp></h2>
-        <h2>JOHN 15:3</h2>
+        <h2><i>You have been made clean already by the teaching I have given you.</i></h2>
+        <h2><i>JOHN 15:3</i></h2>
         <h3 className= "text-light">Needing Jesus Christ Together</h3>
         {/* <img id ="logo" src={tuma} alt="" /> */}
         {/* <HeaderSocial /> */}

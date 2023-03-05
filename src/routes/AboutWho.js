7dcbc6b1d6f7bc7402d9e4191a8dcd01adf2a3cb
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-03-05 07:05:20 +0000

    add ministries section

--- src/routes/AboutWho.js
+++ src/routes/AboutWho.js
@@ -1,19 +1,67 @@
-import Card from 'react-bootstrap/Card';
+import React from 'react'
+import './AboutWho.css'
+import first from '../assets/first.jpeg'
+import {FaHistory} from 'react-icons/fa'
+import {FcApproval} from 'react-icons/fc'
+import {GiProgression} from 'react-icons/gi'
 
-function ImgOverlayExample() {
+const About = () => {
   return (
-    <Card className="bg-dark text-white">
-      <Card.Img src="holder.js/100px270" alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title>Card title</Card.Title>
-        <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
-  );
+    <section id= "About">
+     <h5>Get to know</h5>
+     <h2>About Us</h2>
+
+     <div className="container about_container">
+      <div className="about_me">
+        <div className="about_me-image">
+          <img src={first} alt="about us" />
+        </div>
+      </div>
+       <div className="about_content">
+        <div className= "about_cards">
+          <article className="about_card">
+            <FaHistory className="about_icon"/>
+            <h5>Our History</h5>
+            <small>Our Birth & Goal</small>
+          </article>
+
+          <article className='about_card'>
+            <FcApproval className="about_icon"/>
+            <h5>Mission Statement</h5>
+            <small>Mission & Vision</small>
+          </article>
+
+          <article className='about_card'>
+            <GiProgression className="about_icon"/>
+            <h5>Projects</h5>
+            <small>Current & Future projects</small>
+          </article>
+
+        </div>
+        
+          <h2>Our Vision</h2>
+          <p>To rehabilitate slum dwellers and train them and 
+             other young people for Christian living and ministry
+            through our one year Discipleship program. Offering
+            additional assistance in their spiritual or secular development
+            for those who left the program. 
+            To have a life changing 
+            impact to slum dwellers through teams of young people who
+            evangelize and plant independent churches in the slums of
+             nairobi.
+            To establish social ministries which will meet the needs of slum 
+            dwellers as an act of Christian compassion and as a means to
+            reach them with the gospel.</p>
+            <h2>Our Mission</h2>
+            <p>To establish leader-ships and fellowships of 
+               Christian believers, healthy and stable spiritually, 
+              socially, physically and psychologically in the slums
+              and other areas of need.</p>
+        
+       </div>
+     </div>
+    </section>
+  )
 }
 
-export default ImgOverlayExample;
+export default About;

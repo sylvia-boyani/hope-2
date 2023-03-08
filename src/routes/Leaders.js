5d8ce197fd573353a2cf486034108558924ef21a
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-03-08 14:10:19 +0000

    edit leaders page

--- src/routes/Leaders.js
+++ src/routes/Leaders.js
@@ -4,6 +4,10 @@ import img from '../assets/bish.jpeg'
 import im3 from '../assets/vc.jpeg'
 import im4 from '../assets/James.jpg'
 import im5 from '../assets/pronk.jpeg'
+import im6 from '../assets/maggy.jpeg'
+import im7 from '../assets/peter.jpeg'
+import im8 from '../assets/kelvin.jpeg'
+
 
 
 function Leaders() {
@@ -18,8 +22,8 @@ function Leaders() {
          <div data-aos="fade-left" className="lead-column">
             <div className='lead-content'>
                <h3>Bishop Fred Ogutu Faradays</h3>
-                  <h3>Ministry Leader</h3>
-                  <p>We all have our strengths. I’ve perfected
+               <h4>Ministry Leader and Chairman of the Executive Baord</h4>
+               <p>We all have our strengths. I’ve perfected
                   mine so you can focus on yours. Get in touch
                   to find out how I can help.
                   We all have our strengths. I’ve perfected
@@ -38,13 +42,14 @@ function Leaders() {
                   Each time I take on a new project,
                   I make sure it aligns with my core
                   set of values so I know I can deliver great results.
-                  </p>
+               </p>
             </div>
          </div>
 
          <div data-aos="fade-left" className="lead-column">
             <div className='lead-content'>
                <p><h3>Pastor James Mburu</h3>
+                  <h4>Vice Chairman</h4>
                   My business is more than just a job.
                   Each time I take on a new project,
                   I make sure it aligns with my core
@@ -76,6 +81,7 @@ function Leaders() {
             <div className='lead-content'>
                <p>
                   <h3>Pastor Jameson Wanyama</h3>
+                  <h4>Executive Board General Secretary</h4>
                   Because I'm experienced, reliable,
                   and focused on results, I've had the
                   privilege of working with a host of fantastic clients.
@@ -86,15 +92,46 @@ function Leaders() {
                   privilege of working with a host of fantastic clients.
                   privilege of working with a host of fantastic clients.
                   privilege of working with a host of fantastic clients.
-                  
-                  </p>
+
+               </p>
+            </div>
+         </div>
+         <div data-aos="fade-left" className="lead-column">
+            <div className='lead-content'>
+               <p>
+                  <h3>Kelvin Owuor</h3>
+                  <h4>Executive Board Treasurer</h4>
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+
+               </p>
             </div>
          </div>
 
+         <div data-aos="fade-right" className="lead-column">
+            <div className='lead-img'>
+               <img src={im8} alt="about us" />
+            </div>
+         </div>
 
-          <div data-aos="fade-left" className="lead-column">
+         <div data-aos="fade-right" className="lead-column">
+            <div className='lead-img'>
+               <img src={im5} alt="about us" />
+            </div>
+         </div>
+
+         <div data-aos="fade-left" className="lead-column">
             <div className='lead-content'>
                <p><h3>Pastor Liz Pronk</h3>
+                  <h4>Ministry Sunday School Lead</h4>
                   My business is more than just a job.
                   Each time I take on a new project,
                   I make sure it aligns with my core
@@ -110,11 +147,60 @@ function Leaders() {
             </div>
          </div>
 
+         <div data-aos="fade-left" className="lead-column">
+            <div className='lead-content'>
+               <p>
+                  <h3>Peter Njau</h3>
+                  <h4>Ministry Accountant</h4>
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+
+               </p>
+            </div>
+         </div>
+
          <div data-aos="fade-right" className="lead-column">
             <div className='lead-img'>
-               <img src={im5} alt="about us" />
+               <img src={im7} alt="about us" />
             </div>
-         </div>         
+         </div>
+
+         <div data-aos="fade-right" className="lead-column">
+            <div className='lead-img'>
+               <img src={im6} alt="about us" />
+            </div>
+         </div>
+
+         <div data-aos="fade-left" className="lead-column">
+            <div className='lead-content'>
+               <p>
+                  <h3>Margaret Wanjiku</h3>
+                  <h4>Ministry Youth Leader</h4>
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the Because I'm experienced, reliable,
+                  and focused on results, I've had the
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+                  privilege of working with a host of fantastic clients.
+
+               </p>
+            </div>
+         </div>
       </div>
    )
 }

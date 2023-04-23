24e6c5239aa7d01246b7555afd1ba63c8db25a78
Boyani <sylviaboyani11@gmail.com>
2023-04-23 20:09:58 +0000

    working video links from cloudinary

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -1,9 +1,19 @@
 import "./Gallery.css";
 // import { SRLWrapper } from "simple-react-lightbox";
-const videoPlayer =
-  "https://res.cloudinary.com/ds5ayigjw/video/upload/v1617926559/my-website/2021-04-05_23-07-04_jklscb.mp4";
+const videoPlayer2 =
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682164988/video1_msp4xo.mp4";
+const videoPlayer4 =
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682277540/video4_ookos1.mp4https://res.cloudinary.com/defml0xz0/video/upload/v1682277540/video4_ookos1.mp4https://res.cloudinary.com/defml0xz0/video/upload/v1682277540/video4_ookos1.mp4";
+  const videoPlayer5 =
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682277538/video5_mttjpv.mp4";
+  const videoPlayer6 =
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682277542/video6_nfdlg9.mp4";
+  const videoPlayer7=
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682277531/video7_qugqyn.mp4";
+  const videoPlayer =
+  "https://res.cloudinary.com/defml0xz0/video/upload/v1682277456/bible-70216_fxxmlf.mp4";
 const imageURL =
-  "https://res.cloudinary.com/ds5ayigjw/image/upload/v1618081385/web-site_kc5dsf.png";
+  "https://res.cloudinary.com/defml0xz0/image/upload/v1682278939/events_uwisew.jpg";
 export default function Gallery() {
   return (
     <div className="Gallery">
@@ -16,7 +26,31 @@ export default function Gallery() {
         </div>
         <div className="image-card">
           <video className="img" controls>
-            <source src={videoPlayer} />
+            <source src={videoPlayer7} />
+          </video>
+        </div>
+
+        <div className="image-card">
+          <video className="img" controls>
+            <source src={videoPlayer6} />
+          </video>
+        </div>
+
+        <div className="image-card">
+          <video className="img" controls>
+            <source src={videoPlayer5} />
+          </video>
+        </div>
+
+        <div className="image-card">
+          <video className="img" controls>
+            <source src={videoPlayer5} />
+          </video>
+        </div>
+
+        <div className="image-card">
+          <video className="img" controls>
+            <source src={videoPlayer2} />
           </video>
         </div>
 
@@ -28,13 +62,7 @@ export default function Gallery() {
 
         <div className="image-card">
           <video className="img" controls>
-            <source src={videoPlayer} />
-          </video>
-        </div>
-
-        <div className="image-card">
-          <video className="img" controls>
-            <source src={videoPlayer} />
+            <source src={videoPlayer7} />
           </video>
         </div>
       </div>
